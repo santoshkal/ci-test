5c7550b2e3d78f4fb8ae0ee634f65efc6e970b4c
Santosh <ksantosh@intelops.dev>
2024-07-05 14:09:42 +0530

    Add scrpts

---
+++ .github/workflows/get_version.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Set the working directory
+WORKING_DIR="$HOME/ci-test/dockerfile_policies"
+
+# Navigate to the directory containing the version.env file
+cd "$WORKING_DIR" || { echo "Failed to navigate to $WORKING_DIR"; exit 1; }
+
+# Read the VERSION value from version.env
+VERSION=$(grep '^VERSION=' version.env | cut -d '=' -f 2)
+
+# Output the VERSION value for debugging
+echo "VERSION: $VERSION"
+
+# Set the output for GitHub Actions
+echo "::set-output name=new_version::$VERSION"
